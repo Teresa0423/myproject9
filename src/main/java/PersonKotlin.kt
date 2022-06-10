eb9c72f11f51ab22cec09716f60163a10528189f
Teresa Wnag <teresawang0423@gmail.com>
2022-06-10 16:17:04 +0800

    Hello world with Kotlin

---
+++ src/main/java/PersonKotlin.kt
@@ -0,0 +1,14 @@
+package com.fju
+
+class PersonKotlin {
+    fun greeting(){
+        println("Hello Kotlin.")
+    }
+}
+
+fun main(){
+    var p = PersonKotlin()
+    p.greeting()
+}
+
+/*fun main(args: Array<String>) {}*/
