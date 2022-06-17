55514f13853aa01f25a34b2e63dd57fc8648ac3d
Teresa Wnag <teresawang0423@gmail.com>
2022-06-17 15:16:47 +0800

    Student kotlin

--- src/main/java/com/fju/score/Student.kt
+++ src/main/java/com/fju/score/Student.kt
@@ -1,11 +1,16 @@
 package com.fju.com.fju.score
 
-class Student {
-    var name = ""
-    var english = 0
-    var math = 0
+class Student (var name:String,var english:Int,var math:Int){
+    /*fun print(){
+        println(name+"\t"+english+"\t"+math+"\t")
+    }*/
+    fun print(){
+        println("$name\t$english\t$math\t${getAverage()}") //無法直接拿取的資料要${}
+    }
+    fun getAverage() = (english+math)/2
 }
 
 fun main(){
     val stu = Student("Jack",50,80)
+    println(stu.math)
 }
